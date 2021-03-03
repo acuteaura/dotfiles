0db710d840c6781a34144a3838bfb55d01ba7630
Aurelia Schittler <aurelia@schittler.dev>
2021-03-03 17:22:25 +0100

    add darwin detection

--- home/.config/fish/functions/get_distro.fish
+++ home/.config/fish/functions/get_distro.fish
@@ -1,8 +1,12 @@
 function get_distro
-	for i in (cat /etc/os-release)
-		set arr (echo $i|tr = \n)
-		if [ $arr[1] = "ID" ]
-			echo $arr[2]
+	if [ (uname) = "Darwin" ]
+		echo "darwin"
+	else
+		for i in (cat /etc/os-release)
+			set arr (echo $i|tr = \n)
+			if [ $arr[1] = "ID" ]
+				echo $arr[2]
+			end
 		end
 	end
 end
