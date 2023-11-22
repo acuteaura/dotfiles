f38098d7885d6e28b8403440005c4a3414ce8dba
Aurelia <git@acuteaura.net>
2023-11-22 18:21:12 +0100

    fix WSL stuff

---
+++ dot_config/fish/exact_conf.d/aa_nix.fish
@@ -0,0 +1,3 @@
+if test -f ~/.nix-profile/etc/profile.d/nix.fish
+    source /home/aurelia/.nix-profile/etc/profile.d/nix.fish
+end
---
+++ dot_config/fish/exact_conf.d/ssh_wsl.fish
@@ -0,0 +1,3 @@
+if test -S ~/.local/state/1password-agent.sock
+    set -x SSH_AUTH_SOCK ~/.local/state/1password-agent.sock
+end
