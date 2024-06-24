6854a7885e45cf1a24ff2c0aa268804b127cbd8b
Aurelia <git@acuteaura.net>
2024-06-24 19:24:32 +0200

    refactor: ssh agent

--- dot_config/fish/exact_conf.d/ssh_op.fish
+++ dot_config/fish/exact_conf.d/ssh_op.fish
@@ -1,3 +1,7 @@
 if test -S ~/.local/state/1password-agent.sock
     set -x SSH_AUTH_SOCK ~/.local/state/1password-agent.sock
 end
+
+if test -S ~/.1password/agent.sock
+    set -x SSH_AUTH_SOCK ~/.1password/agent.sock
+end
