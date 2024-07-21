3325c41b9e4fe4f9198469e6743f7c42bb1ae432
Aurelia <git@acuteaura.net>
2024-07-21 18:31:52 +0200

    Update .config/fish/conf.d/aa_nix.fish

--- dot_config/fish/exact_conf.d/aa_nix.fish
+++ dot_config/fish/exact_conf.d/aa_nix.fish
@@ -1,3 +1,5 @@
-if test -f ~/.nix-profile/etc/profile.d/nix.fish
+if test -f /nix/var/nix/profiles/default/etc/profile.d/nix.fish
+    source /nix/var/nix/profiles/default/etc/profile.d/nix.fish
+else if test -f ~/.nix-profile/etc/profile.d/nix.fish
     source /home/aurelia/.nix-profile/etc/profile.d/nix.fish
 end
