f8b78942e9aedd52267d9c8d91322053880ba967
Aurelia <git@acuteaura.net>
2024-11-17 23:42:59 +0100

    refactor: age aliases

---
+++ dot_config/fish/functions/age-decrypt.fish
@@ -0,0 +1,3 @@
+function age-decrypt
+    age -d -i (op read op://agoz6xpan4yq6zkpzmfipbvab4/wb2fr5ug3vhgodkdjzrhmd4wnu/.age | psub -F) $argv
+end
---
+++ dot_config/fish/functions/age-encrypt.fish
@@ -0,0 +1,3 @@
+function age-encrypt
+    age -r (op item get wb2fr5ug3vhgodkdjzrhmd4wnu --fields "public key") $argv
+end
