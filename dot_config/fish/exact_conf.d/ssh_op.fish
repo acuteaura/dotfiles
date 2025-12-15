10a710d7c1aaeb15a8d5285d880588bcdc02bd5a
Aurelia <git@acuteaura.net>
2025-12-15 01:47:26 +0100

    Update .config/fish/conf.d/ssh_op.fish

--- dot_config/fish/exact_conf.d/ssh_op.fish
+++ dot_config/fish/exact_conf.d/ssh_op.fish
@@ -1,7 +1,9 @@
+if not set -q SSH_AUTH_SOCK
 if test -S ~/.local/state/1password-agent.sock
     set -x SSH_AUTH_SOCK ~/.local/state/1password-agent.sock
 end
 
 if test -S ~/.1password/agent.sock
     set -x SSH_AUTH_SOCK ~/.1password/agent.sock
+end
 end
