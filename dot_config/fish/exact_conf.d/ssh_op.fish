05d26f24332b6d533c007f1dae07408cb52e2f16
Aurelia <git@acuteaura.net>
2026-02-01 13:48:47 +0100

    detect macOS shenanigans in ssh_op

--- dot_config/fish/exact_conf.d/ssh_op.fish
+++ dot_config/fish/exact_conf.d/ssh_op.fish
@@ -1,9 +1,13 @@
-if not set -q SSH_AUTH_SOCK
-if test -S ~/.local/state/1password-agent.sock
-    set -x SSH_AUTH_SOCK ~/.local/state/1password-agent.sock
-end
+if not set -q SSH_AUTH_SOCK; or string match -q '/private/tmp/com.apple.launchd.*/Listeners' "$SSH_AUTH_SOCK"
+    if test -S ~/.local/state/1password-agent.sock
+        set -x SSH_AUTH_SOCK ~/.local/state/1password-agent.sock
+    end
 
-if test -S ~/.1password/agent.sock
-    set -x SSH_AUTH_SOCK ~/.1password/agent.sock
-end
+    if test -S ~/.1password/agent.sock
+        set -x SSH_AUTH_SOCK ~/.1password/agent.sock
+    end
+
+    if test -S ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+        set -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+    end
 end
